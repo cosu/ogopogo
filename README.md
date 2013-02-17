251cf627fac3e28150d12d6a346e4ee7f7d74108
Cosmin Dumitru <cosu@cosu.ro>
2013-02-17 22:11:33 +0100

    - readme - comments in buildroot_fs

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
-Ogopogo - A simple environment  for running network experiments 
+Ogopogo - quickly build virtual network topologies.
 =========================
-
-Ogopogo allows one to quickly build virtual network topologies.  
 
 Requirements
 ------------
 Python, user-mode-linux, screen
-
+Optionally python-networkx and python-matplotlib
 
 Getting started
 ---------------
 - write a cfg file using .ini/properties format (see /examples)
-- start network ./ogo.py start config.cfg  - starts the topoligy
+- start network ./ogo.py start config.cfg  - starts the topology
 - stop network ./ogo.py stop config.cfg  - stops the topology
-- map network ./ogo.py map config.cfg  - this creats a png file of the network topology - useful for debugging setups
+- map network ./ogo.py map config.cfg  - this creates a png file of the network topology - useful for debugging setups
 
 Once the network is started you can attach to the console of each device using the screen utility:
 
@@ -30,10 +28,9 @@ Once the network is started you can attach to the console of each device using t
         24036.host6	(02/16/12 10:43:58)	(Detached)
         23664.sniffer1	(02/16/12 10:43:53)	(Detached)
 
-Now to connect to host1
+Connect to host1
 
     root@uml-host:~# screen -r host1
-
 
 
 Features
@@ -42,14 +39,17 @@ Features
 ###Creating Devices
 The script can create three types of network devices:
 
-- switches: UML switches to which the other devices connect. All switches are started in -hub mode.
+- switches: UML switches to which the other devices connect. All switches are started in -hub mode so network sniffers can be attached.
 - hosts: UML instances connected to the switches. Useful to test pings, services, etc.
 - sniffers: they dump via tcpump to $UML_HOME all captured traffic. You can connect to multiple network segments as each segment is dumped to a separate file.
-- bridges: using bridge-tools this device bridges two or more ethernet interfaces. One instance supports more than one bridge
-- routers:  routers load quagga config files via hostfs and start the quagga routing suite
+- bridges: using bridge-tools this device bridges two or more ethernet interfaces. One instance supports multiple bridges
+- routers:  routers load quagga config files via hostfs and start the quagga routing suite. See the examples dir for more detail
 
 ###Drawing
-As a debug feature one can draw the network topologies resulted from the config files. This is done via the map argument
-: ./ogo.py map simple.cfg. It's automated so the layout could be better but it still gives a good overview of the network
+As a debug feature one can draw the network topologies resulted from the config files. This is done via the map argument.
+
+    ./ogo.py map simple.cfg
+
+It's automated so the layout could be better but it still gives a good overview of the network
 
 
--- scripts/build_rootfs.sh
+++ scripts/build_rootfs.sh
@@ -1,33 +1,46 @@
 #!/bin/bash
 
-#list of extra packages to be installed
-PACKAGES="tcpdump quagga bridge-utils radvd bird"
-BASE_PACKAGES="iproute net-tools iputils-ping traceroute mingetty module-init-tools procps vim"
+#author: Cosmin 'cosu' Dumitru <c.dumitru@uva.nl>
+
+#This script builds a ready to run User mode linux image. A file called rc.local should be present next to this script to
+# enable extra functionality. This script must be run as root. If you decide to run the UML machine as a regular user
+# make sure you change the permisions of the image as the resulting image file will be owned by root
+
+#========Config
 #image size. note that the image is created as a sparse file
 SIZE=2048
-
-
 #image name
 IMG_NAME=disk.img
-
-
-#tmpdir used to mount the image for debootrapping
-TMPDIR=/mnt
+#list of extra packages to be installed
+SERVICES="quagga radvd bird"
+PACKAGES="tcpdump bridge-utils"
+BASE_PACKAGES="iproute net-tools iputils-ping traceroute mingetty module-init-tools procps vim"
 #to speedup installation use a nearby mirror
 MIRROR="http://ftp.nl.debian.org/debian"
+#=======Config ends here
 
 
+#tmpdir used to mount the image for debootstrap
+TMPDIR=/mnt
+
+
+#check for root
 if [ `whoami` != root ]; then
     echo Please run this script as root or using sudo
     exit
 fi
 
+
+#build image file
 dd if=/dev/zero of=$IMG_NAME bs=1M count=1 seek=$SIZE
 mkfs.ext3 -F $IMG_NAME
 mount -o loop $IMG_NAME $TMPDIR
 
-debootstrap --variant=minbase --include="$PACKAGES $BASE_PACKAGES "  stable $TMPDIR $MIRROR
 
+#install base system
+debootstrap --variant=minbase --include="$PACKAGES $BASE_PACKAGES $SERVICES "  stable $TMPDIR $MIRROR
+
+#configure uml modules via hostfs
 echo "hostfs /lib/modules hostfs /usr/lib/uml/modules 0 0" >> $TMPDIR/etc/fstab
 echo "proc /proc proc defaults 0 0
 tmpfs                  /tmp          tmpfs     defaults          0      0
@@ -37,24 +50,32 @@ tmpfs                  /var/run          tmpfs     defaults          0      0
 tmpfs                  /var/tmp          tmpfs     defaults         0      0
 " >> $TMPDIR/etc/fstab
 
-for service in quagga radvd cron bird; do
+#disable services at startup. we'll start them in a role based fashion later
+for service in $SERVICES; do
   chroot $TMPDIR /bin/bash -c "update-rc.d -f $service remove" 2>/dev/null
 done
 
-chroot $TMPDIR /bin/bash -c "mkdir /lib/modules"
+#remove some services to speedup startup
 mv $TMPDIR/etc/rcS.d/S04hwclockfirst.sh $TMPDIR/etc/rcS.d/K04hwclockfirst
 mv $TMPDIR/etc/rcS.d/S06hwclock.sh $TMPDIR/etc/rcS.d/K06hwclock.sh
 mv $TMPDIR/etc/rcS.d/S06module-init-tools $TMPDIR/etc/rcS.d/K08module-init-tools
+
+#copy our own version of rc.local to enable roles
 cp rc.local $TMPDIR/etc/
 chmod +x $TMPDIR/etc/rc.local
 
+#backup inittab
 cp $TMPDIR/etc/inittab $TMPDIR/etc/inittab.save
+#modify inittab for root autologin
 grep -v "getty" $TMPDIR/etc/inittab.save > $TMPDIR/etc/inittab
 echo "1:2345:respawn:/sbin/mingetty --autologin root --noclear tty0" >> $TMPDIR/etc/inittab
 
+#allow connections from the local console
 echo "tty0" >> $TMPDIR/etc/securetty
 
+#basic network config
 echo "auto lo" >> $TMPDIR/etc/network/interfaces
 echo "iface lo inet loopback" >> $TMPDIR/etc/network/interfaces
+
 
 umount $TMPDIR
